8c02d9b62a142ca2a1c0bbbf27eaa618cae06e8d
Danny Hwang <dannyhwangca@gmail.com>
2020-04-18 21:20:56 -0700

    updated App.js base

--- reactinterface/src/components/App.js
+++ reactinterface/src/components/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import '../css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <main class="page bg-white" id="petratings">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 bg-white">
+              <div class="container">
+                <div>Add Appointments</div>
+                <div>Search Appointments</div>
+                <div>List Appointments</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    )
+  }
 }
 
 export default App;
