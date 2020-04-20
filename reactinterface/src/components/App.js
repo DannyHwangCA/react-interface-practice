14302d4852ae5704635daf891e97d2ea8e1a788e
Danny Hwang <dannyhwangca@gmail.com>
2020-04-19 20:36:10 -0700

    added form processing

--- reactinterface/src/components/App.js
+++ reactinterface/src/components/App.js
@@ -16,6 +16,7 @@ class App extends Component {
     }
     this.deleteAppointment = this.deleteAppointment.bind(this);
     this.toggleForm = this.toggleForm.bind(this);
+    this.addAppointment = this.addAppointment.bind(this);
   }
 
   deleteAppointment(apt) {
@@ -25,6 +26,16 @@ class App extends Component {
      this.setState({
        myAppointments: tempApts
      })
+  }
+
+  addAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    apt.aptId = this.state.lastIndex;
+    tempApts.unshift(apt);
+    this.setState({
+      myAppointments: tempApts,
+      lastIndex: this.state.lastIndex + 1
+    })
   }
 
   toggleForm() {
@@ -63,6 +74,7 @@ class App extends Component {
                 <AddAppointments 
                   formDisplay = {this.state.formDisplay}
                   toggleForm = {this.toggleForm}
+                  addAppointment = {this.addAppointment}
                 />
                 <SearchAppointments />
                 <ListAppointments 
